e1410e348d1bb6203de463476e288facd8881858
davidemerli <davide.merli@outlook.com>
2020-12-05 12:08:21 +0100

    refactor: more elegant solution

--- 2020/day5/main.py
+++ 2020/day5/main.py
@@ -1,44 +1,21 @@
-from functools import reduce
 import aocd
+import re
 
 DAY, YEAR = 5, 2020
-ids = []
 
 
 def data():
+    global ids
     data = aocd.get_data(day=DAY, year=YEAR).splitlines()
-    data = [l for l in data]
-
-    return data
-
-
-def bin(begin, end, mapping, string, stop_at):
-    mid = (begin + end) // 2
-
-    if len(string) == 1:
-        return begin if string[0] == mapping[0] else end
-    elif string[0] == mapping[0]:
-        return bin(begin, mid, mapping, string[1:], stop_at)
-    elif string[0] == mapping[1]:
-        return bin(mid + 1, end, mapping, string[1:], stop_at)
+    ids = [int(re.sub(r'B|R', '1', re.sub(r'F|L', '0', d)), 2) for d in data]
+    return ids
 
 
 def first():
-    global ids
-
-    def id(r, c):
-        return r * 8 + c
-
-    for d in data():
-        r = bin(0, 127, ('F', 'B'), d[:-3], 4)
-        c = bin(0, 7, ('L', 'R'), d[-3:], 1)
-        ids.append(id(r, c))
-
-    return max(ids), ids
+    return max(data())
 
 
 def second():
-    global ids
     return set(i for i in range(min(ids), max(ids) + 1)).difference(set(ids)).pop()
 
 
