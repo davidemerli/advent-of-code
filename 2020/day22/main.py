91be38284f1aa700d3554edfaf0dcf21ea5e22ab
Davide Merli <davide.merli@outlook.com>
2020-12-24 15:29:03 +0100

    feat: day22

--- 2020/day22/main.py
+++ 2020/day22/main.py
@@ -5,7 +5,6 @@ DAY, YEAR = 22, 2020
 
 def data():
     data = aocd.get_data(day=DAY, year=YEAR).split('\n\n')
-    # data = open('in').read().split('\n\n')
     return [list(map(int, d.splitlines()[1:])) for d in data]
 
 
@@ -27,8 +26,48 @@ def first():
 
     winner = p2 if len(p1) == 0 else p1
 
+    return score(winner)
+
+
+def score(winner):
     return sum((i + 1) * val for i, val in enumerate(winner[::-1]))
 
 
+def play_game(p1, p2):
+    turns = []
+
+    while len(p1) != 0 and len(p2) != 0:
+        if [p1, p2] in turns:
+            return (0, p1)
+
+        turns.append([p1[:], p2[:]])
+
+        c1, c2 = p1.pop(0), p2.pop(0)
+
+        if c1 > len(p1) or c2 > len(p2):
+            if c1 > c2:
+                p1 += [c1, c2]
+            else:
+                p2 += [c2, c1]
+        else:
+            round_won = play_game(p1[:c1], p2[:c2])
+
+            if round_won[0] == 0:
+                p1 += [c1, c2]
+            else:
+                p2 += [c2, c1]
+
+    winner = (1, p2) if len(p1) == 0 else (0, p1)
+    return winner
+
+
+def second():
+    p1, p2 = data()
+
+    _, winner_deck = play_game(p1, p2)
+
+    return score(winner_deck)
+
+
 aocd.submit(first(), part='a', day=DAY, year=YEAR)
-# aocd.submit(second(), part='b', day=DAY, year=YEAR)
+aocd.submit(second(), part='b', day=DAY, year=YEAR)
