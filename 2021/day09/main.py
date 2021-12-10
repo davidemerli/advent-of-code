55464aad3514c8b222b9185c6a2a8ac3d1ec952d
davidemerli <davide.merli@outlook.com>
2021-12-10 02:06:21 +0100

    feat: day09 part 2

--- 2021/day09/main.py
+++ 2021/day09/main.py
@@ -1,10 +1,11 @@
+from functools import reduce
+
 data = [[int(x) for x in line.strip()] for line in open("input.txt").readlines()]
 
-dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+dirs = {(1, 0), (0, 1), (-1, 0), (0, -1)}
 
 
 def neighbours(x, y):
-
     return [
         (x + dx, y + dy)
         for dx, dy in dirs
@@ -13,7 +14,7 @@ def neighbours(x, y):
 
 
 def is_lowpoint(x, y):
-    return all(data[x][y] < data[n[0]][n[1]] for n in neighbours(x, y))
+    return all(data[x][y] < data[a][b] for a, b in neighbours(x, y))
 
 
 def part1():
@@ -33,8 +34,25 @@ def part2():
         if is_lowpoint(x, y)
     ]
 
-    print(len(low_points))
+    def find_basin(x, y):
+        to_visit = [(x, y)]
+
+        basin = set()
+
+        while to_visit:
+            x, y = to_visit.pop(0)
+            level = data[x][y]
+
+            basin.add((x, y))
+
+            to_visit += [(a, b) for a, b in neighbours(x, y) if level < data[a][b] < 9]
+
+        return basin
+
+    return reduce(
+        lambda a, b: a * b, sorted(len(find_basin(x, y)) for x, y in low_points)[-3:]
+    )
 
 
-# print(part1())
+print(part1())
 print(part2())
